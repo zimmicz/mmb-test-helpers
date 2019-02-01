134d513d882da741d0e9430ec739a68eec80122d
Michal Zimmermann <zimmicz@gmail.com>
2019-02-01 12:49:42 +0100

    Use config dict

--- mmb_test_helpers/authbasetest.py
+++ mmb_test_helpers/authbasetest.py
@@ -2,7 +2,7 @@ from .basetest import BaseTestCase
 from .setup_login import login
 
 class AuthBaseTestCase(BaseTestCase):
-    def setUp(self, app, login_username, login_password, conn_string, sql_file):
-        super(AuthBaseTestCase, self).setUp(app, conn_string, sql_file)
-        self.headers = login(login_username, login_password)
+    def setUp(self, app, settings):
+        super(AuthBaseTestCase, self).setUp(app, settings['conn_string'], settings['sql_file'])
+        self.headers = login(settings['login_username'], settings['login_password'])
 
--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 import setuptools
 
 setuptools.setup(name='mmb_test_helpers',
-      version='0.1',
+      version='0.2',
       description='mapmybook test helpers',
       url='http://github.com/zimmicz/map-my-book-test-helpers',
       author='Michal Zimmermann',
       author_email='zimmi@tutanota.com',
       license='MIT',
       packages=setuptools.find_packages(),
+      install_requires=['flask', 'requests', 'psycopg2'],
       zip_safe=False)
 
