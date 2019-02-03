936a6a701d7aff18a696c2e6f4019156f79f935b
Michal Zimmermann <zimmicz@gmail.com>
2019-02-03 12:09:27 +0100

    Fix setUp params

--- mmb_test_helpers/basetest.py
+++ mmb_test_helpers/basetest.py
@@ -2,7 +2,7 @@ import unittest
 from . import setup_db
 
 class BaseTestCase(unittest.TestCase):
-    def setUp(self, app, config):
+    def setUp(self, app, settings):
         """
         Refresh database.
 
@@ -12,5 +12,5 @@ class BaseTestCase(unittest.TestCase):
         """
         self.app = app
         self.client = self.app.test_client
-        setup_db.run_file(config['conn_string'], config['sql_file'])
+        setup_db.run_file(settings['conn_string'], settings['sql_file'])
 
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(name='mmb_test_helpers',
-      version='0.2.5',
+      version='0.2.6',
       description='mapmybook test helpers',
       url='http://github.com/zimmicz/map-my-book-test-helpers',
       author='Michal Zimmermann',
