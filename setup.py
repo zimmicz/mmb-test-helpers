0adc955627b81778519201660295e358e174f5f0
Michal Zimmermann <zimmicz@gmail.com>
2019-02-03 12:14:35 +0100

    Raise version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
 setuptools.setup(name='mmb_test_helpers',
-      version='0.2.6',
+      version='0.2.7',
       description='mapmybook test helpers',
       url='http://github.com/zimmicz/map-my-book-test-helpers',
       author='Michal Zimmermann',
