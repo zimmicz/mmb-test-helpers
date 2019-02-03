e337bf162c545c0f474c7ff7b64b37f88958d956
Michal Zimmermann <zimmicz@gmail.com>
2019-02-03 12:12:57 +0100

    Fix setUp params

--- mmb_test_helpers/authbasetest.py
+++ mmb_test_helpers/authbasetest.py
@@ -3,6 +3,6 @@ from .setup_login import login
 
 class AuthBaseTestCase(BaseTestCase):
     def setUp(self, app, settings):
-        super(AuthBaseTestCase, self).setUp(app, settings['conn_string'], settings['sql_file'])
+        super(AuthBaseTestCase, self).setUp(app, settings)
         self.headers = login(settings['login_username'], settings['login_password'])
 
