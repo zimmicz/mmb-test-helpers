6696ce63878795eb5529362deda80772baaaf3fa
Michal Zimmermann <zimmicz@gmail.com>
2020-02-28 13:12:05 +0100

    Fix login method

--- mmb_test_helpers/authbasetest.py
+++ mmb_test_helpers/authbasetest.py
@@ -4,5 +4,5 @@ from .setup_login import login
 class AuthBaseTestCase(BaseTestCase):
     def setUp(self, app, settings):
         super(AuthBaseTestCase, self).setUp(app)
-        self.headers = login(settings['login_username'], settings['login_password'])
+        self.headers = login(settings['login_url'], settings['login_username'], settings['login_password'])
 
